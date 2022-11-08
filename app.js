6e9e8b917083b9adc956eba11ce33f73e3c66ea0
Aaron Ibarra <aaronibarra132@gmail.com>
2022-11-08 11:57:23 -0800

    clicking on enemies alerts user and updates state depending on math random

--- app.js
+++ app.js
@@ -1,17 +1,50 @@
 /* Imports */
+import { renderEnemy } from './render-utils.js';
 
 /* Get DOM Elements */
-let hitPointEl = document.querySelector("#hit-points");
+const hitPointEl = document.querySelector('#hit-points');
+const enemiesEl = document.querySelector('.enemies');
 
 /* State */
 let hitPoints = 10;
-let enemies = [
-    {id: 1, name: 'Ganon', hp: 3 }
-    {id: 2, name: 'Stinker', hp: 1 }
+let defeatedEnemies = 0;
+let currentId = 4;
+const enemies = [
+    { id: 1, name: 'Ganon', hp: 3 },
+    { id: 2, name: 'Stinker', hp: 1 },
 ];
 
 /* Events */
+function enemyHandler(enemy) {
+    if (enemy.hp <= 0) return;
+
+    if (Math.random() < 0.5) {
+        enemy.hp--;
+        alert(`You hit ${enemy.name}! NOICE!!!`);
+    } else {
+        alert('Lanky MISSED. What will Zolder think of you now...');
+    }
+
+    if (Math.random() < 0.5) {
+        hitPoints--;
+        alert(`Aw no! AEEYUHUHAUUH. ${enemy.name} slapped you across the face :/`);
+    } else {
+        alert(`${enemy.name} tried to hit you but MISSED. Nice roll Lanky :)`);
+    }
+}
 
 /* Display Functions */
+function displayEnemies() {
+    enemiesEl.textContent = '';
+
+    for (let enemy of enemies) {
+        const currentEnemy = renderEnemy(enemy);
+        currentEnemy.addEventListener('click', () => {
+            enemyHandler(enemy);
+        });
+        enemiesEl.append(currentEnemy);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
+displayEnemies();
---
+++ render-utils.js
@@ -0,0 +1,16 @@
+export function renderEnemy(enemy) {
+    const enemyEl = document.createElement('div');
+    const nameEl = document.createElement('p');
+    const hpEl = document.createElement('p');
+    const appearanceEl = document.createElement('div');
+
+    //appearanceEl.classList.add('bat');
+
+    nameEl.textContent = enemy.name;
+
+    hpEl.textContent = enemy.hp < 0 ? 0 : enemy.hp;
+
+    enemyEl.append(appearanceEl, hpEl, nameEl);
+
+    return enemyEl;
+}
